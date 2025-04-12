eef2fdc2205fc640378a94d3268cab0c0f5615a1
sluder-ta <sluder.ta@gmail.com>
2025-04-12 15:37:54 -0400

    update exercise 16 to read written text and close files

--- lp3thw/e16.py
+++ lp3thw/e16.py
@@ -11,7 +11,7 @@ input("?")
 print("Opening the file...")
 target = open(filename, 'w')
 
-print("truncating the file. Goodbye!")
+print("Truncating the file. Goodbye!")
 target.truncate()
 
 print("Now I'm going to ask you for three lines.")
@@ -22,12 +22,12 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(f"{line1}\n{line2}\n{line3}\n")
 
 print("And finally, we close it.")
 target.close()
+
+print("Here's what you wrote in the file: ")
+read_it = open(filename)
+print(read_it.read())
+read_it.close()
