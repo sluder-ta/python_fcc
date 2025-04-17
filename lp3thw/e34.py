cafe8e0266263120038b84cda46bd7850bf710b8
sluder-ta <sluder.ta@gmail.com>
2025-04-17 19:40:44 -0400

    complete lp3thw exercise 34

---
+++ lp3thw/e34.py
@@ -0,0 +1,19 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#1. The animal at 1. The animal at 1 is the 2nd anial and is a python.
+#2. The third (3rd) animal. The third (3rd) animal is at 2 and is a peacock.
+#3. The first (1st) animal. The first (1st) animal is at 0 and is a bear.
+#4. The animal at 3. The animal at 3 is the 4th animal and is a kangaroo.
+#5. The fifth (5th) animal. The fifth (5th) animal is at 4 and is a whale.
+#6. The animal at 2. The animal at 2 is the 3rd animal and is a peacock.
+#7. the sixth (6th) animal. The sixth (6th) animal is at 5 and is a platypus.
+#8. The animal at 4. The animal at 4 is the 5th animal and is a whale.
+
+print(animals[1])
+print(animals[2])
+print(animals[0])
+print(animals[3])
+print(animals[4])
+print(animals[2])
+print(animals[5])
+print(animals[4])
